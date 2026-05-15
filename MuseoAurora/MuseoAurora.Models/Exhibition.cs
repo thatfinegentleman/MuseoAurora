5ae725d0a018c610ad3b99788773d292504276a9
magnemagne <125281713+magnemagne@users.noreply.github.com>
2026-05-15 14:52:33 +0200

    More models

---
+++ MuseoAurora/MuseoAurora.Models/Exhibition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MuseoAurora.Models
+{
+    public class Exhibition
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string ImageUrl { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ MuseoAurora/MuseoAurora.Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MuseoAurora.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public int VisitorId { get; set; }
+        public int TicketTypeId { get; set; }
+        public int ExhibitionId { get; set; }
+        public int GuidedTourId { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public TimeSpan PurchaseDate { get; set; }
+    }
+}
